dbc516412362658727107f28fbbd8c0bb939a10a
Edward Ybarra <edwardcybarra@gmail.com>
2021-11-12 15:54:17 -0600

    added canSum and staring howSum

---
+++ how-sum.js
@@ -0,0 +1,12 @@
+const howSum = (targetSum, numbers) => {
+    
+}
+
+
+
+
+console.log(canSum(7, [2, 3]));
+console.log(canSum(7, [5, 3, 4, 7]));
+console.log(canSum(7, [2, 4]));
+console.log(canSum(8, [2, 3, 5]));
+console.log(canSum(300, [7, 14]));
