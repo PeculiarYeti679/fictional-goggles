b463417bf3d4730738053dd44a69ce5116888a27
Edward Ybarra <edwardcybarra@gmail.com>
2021-11-12 23:39:16 -0600

    canConstruct brute force method

---
+++ can-construct.js
@@ -0,0 +1,19 @@
+const canConstruct = (target, wordBank) => {
+
+    if (target === '') return true;
+
+    for (let word of wordBank) {
+        if (target.indexOf(word) === 0) {
+            const suffix = target.slice(word.length);
+            if (canConstruct(suffix, wordBank) === true) {
+                return true;
+            }
+        }
+    }
+    return false;
+};
+
+console.log(canConstruct("abcedf", ["ab", "abc", "cd", "def", "abcd"]));
+console.log(canConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"]));
+console.log(canConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "t"]));
+console.log(canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", ["e", "ee", "eee", "eeee", "eeeee", "eeeeee"]));
